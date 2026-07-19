56f3dd5273b98b340bd2fc467b962652fe8c347a
hatem <hatemhessan969@gmail.com>
2026-07-19 14:13:38 +0300

    Add project files.

---
+++ E_Commerce.Infrastructure/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Data.Configurations
+{
+    internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.ProductType)
+                   .WithMany()
+                   .HasForeignKey(p => p.TypeId);
+
+            builder.HasOne(p => p.ProductBrand)
+                   .WithMany()
+                   .HasForeignKey(p => p.BrandId);
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.Description).HasMaxLength(500);
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+            builder.Property(p => p.PictureUrl).IsRequired().HasMaxLength(500);
+        }
+    }
+}
