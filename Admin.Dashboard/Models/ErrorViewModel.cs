e1583c6bfd145dc211e2de065d93f4d0cdb9c5af
Hatem Hussein <152395771+hatem32@users.noreply.github.com>
2026-09-05 19:18:02 +0300

    Delete Admin.Dashboard directory

--- Admin.Dashboard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Admin.Dashboard.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Admin.Dashboard/wwwroot/js/datatables-simple-demo.js
+++
@@ -1,9 +0,0 @@
-﻿window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
-});
